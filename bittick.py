c1cd1eb71f78e78ecb24cb2f770133818bdba3c6
psz <36031831+pszlo@users.noreply.github.com>
2022-01-16 17:08:22 +0300

    Add files via upload

---
+++ bittick.py
@@ -0,0 +1,17 @@
+pokt77=[0];yjj2=[0];
+def t77t():
+   while True:
+    time.sleep(10);pokt77[-1]+=1;
+    try:
+     bitcoin777.append(float(client7.get_margin_price_index(symbol='BTCUSDT')['price']));
+     etherium.append(float(client7.get_margin_price_index(symbol='ETHUSDT')['price']));
+     bitc79.append((etherium[-1],bitcoin777[-1]));
+     bitcoin5();
+    except: yjj2.append(len(bitcoin777));
+     
+     
+
+pokt77=[0];
+h77t = threading.Thread(target=t77t)
+h77t.daemon = True
+h77t.start()
